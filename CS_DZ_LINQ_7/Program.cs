197fdcb16acbe5cd173fec98bd4337536be9f9da
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-03-23 16:53:07 +0500

    Добавьте файлы проекта.

---
+++ CS_DZ_LINQ_7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ_LINQ_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Soldier> soldiers1 = new List<Soldier> { new Soldier("Балабанов", "Олег"), new Soldier("Алексеев", "Антон"), 
+                new Soldier("Егоров", "Егор"), new Soldier("Бритых", "Игорь")};
+
+            List<Soldier> soldiers2 = new List<Soldier> { new Soldier("Игнатьев", "Роман"), new Soldier("Мытых", "Артем"), 
+                new Soldier("Калашников", "Калаш"), new Soldier("Летчиков", "Валентин")};
+
+            var filteredSoldiers1 = soldiers1.Where(soldier => soldier.LastName.StartsWith("Б"));
+            var newSoldiers2 = filteredSoldiers1.Union(soldiers2);
+
+            Console.WriteLine("Новый список отряда 2: ");
+
+            foreach (var soldier in newSoldiers2)
+            {
+                Console.WriteLine(soldier.LastName + " " + soldier.Name);
+            }
+        }
+    }
+
+    class Soldier
+    {
+        public string LastName { get; private set; }
+
+        public string Name { get; private set; }
+
+        public Soldier(string lastName, string name)
+        {
+            LastName = lastName;
+            Name = name;
+        }
+    }
+}
